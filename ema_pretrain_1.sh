695e1ac8705482b81b17f58b2f60a4f305716e43
guangheli12 <3280939942@qq.com>
2024-04-17 11:29:55 +0000

    fix ignore

---
+++ ema_pretrain_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# tau = 0.1, 0.3 
+
+python ema_bootstrap_pretrain.py --tau=0.1 --warmup_target_epochs=1 --output_dir=bootstrap_pretrain_weights --log_dir=bootstrap_pretrain_logs --device=cuda:1
+python ema_bootstrap_pretrain.py --tau=0.3 --warmup_target_epochs=1 --output_dir=bootstrap_pretrain_weights --log_dir=bootstrap_pretrain_logs --device=cuda:1
+
+python ema_bootstrap_pretrain.py --tau=0.1 --warmup_target_epochs=5 --output_dir=bootstrap_pretrain_weights --log_dir=bootstrap_pretrain_logs --device=cuda:1
+python ema_bootstrap_pretrain.py --tau=0.3 --warmup_target_epochs=5 --output_dir=bootstrap_pretrain_weights --log_dir=bootstrap_pretrain_logs --device=cuda:1
