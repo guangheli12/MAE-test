6c7e756c7c9abfdd18fc65a16684b02948e1593d
guangheli12 <3280939942@qq.com>
2024-04-18 11:25:42 +0800

    add new method

---
+++ new_pretrain.py
@@ -0,0 +1,269 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+import argparse
+import datetime
+import json
+import numpy as np
+import os
+import time
+from pathlib import Path
+from tqdm import tqdm 
+import torchvision 
+import torch
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import timm
+
+# assert timm.__version__ == "0.3.2"  # version check
+import timm.optim.optim_factory as optim_factory
+
+import util.misc as misc
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
+
+import models_mae
+
+
+from engine_pretrain_bootstrap import train_one_epoch_new 
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
+    parser.add_argument('--batch_size', default=512, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+
+    # default is 200 epochs 
+    parser.add_argument('--slow_lambda', default = 1, type = int) 
+    parser.add_argument('--epochs', default=200, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int,
+                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+    # Model parameters (original)
+    parser.add_argument('--model', default='mae_deit_tiny', type=str, metavar='MODEL',
+                        help='Name of model to train')
+    # Name of target model 
+    parser.add_argument('--target_model', default = 'mae_deit_tiny_target', type = str)  
+
+    # CIFAR10 has size of 32 
+    parser.add_argument('--input_size', default=32, type=int,
+                        help='images input size')
+
+    parser.add_argument('--mask_ratio', default=0.75, type=float,
+                        help='Masking ratio (percentage of removed patches).')
+
+    parser.add_argument('--norm_pix_loss', action='store_true',
+                        help='Use (per-patch) normalized pixels as targets for computing loss')
+    parser.set_defaults(norm_pix_loss=False)
+
+    # Optimizer parameters
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')  
+    
+    # 这个是 EMA 的系数，即 new_weight * tau + old_weight * (1 - tau) 
+    parser.add_argument('--tau', type = float, default = 0.005)
+
+
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+
+    parser.add_argument('--warmup_epochs', type=int, default=40, metavar='N',
+                        help='epochs to warmup LR')
+
+    parser.add_argument('--num_k', type = int) 
+    # Dataset parameters       
+    parser.add_argument('--output_dir', default='./bootstrap_pretrain_weights',
+                        help='path where to save, empty for no saving')
+    
+    # tensorboard logouts here 
+    parser.add_argument('--log_dir', default='./bootstrap_logs',
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    return parser
+
+
+def main(args):
+    misc.init_distributed_mode(args)
+
+    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
+    print("{}".format(args).replace(', ', ',\n'))
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    cudnn.benchmark = True
+
+    # simple augmentation
+    transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(args.input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])    
+    dataset_train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    print(dataset_train)
+
+
+
+    # only have on GPU 
+    sampler_train = torch.utils.data.RandomSampler(dataset_train)
+
+    # if global_rank == 0 and args.log_dir is not None:
+    os.makedirs(args.log_dir, exist_ok=True)
+    log_writer = SummaryWriter(log_dir=args.log_dir)
+    # else:
+        # log_writer = None
+
+    data_loader_train = torch.utils.data.DataLoader(
+        dataset_train, sampler=sampler_train,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
+        drop_last=True,
+    )
+    
+    # define the model
+    model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss)
+    model.to(device)
+
+
+    model_without_ddp = model
+    print("Model = %s" % str(model_without_ddp))
+
+    eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
+    
+    if args.lr is None:  # only base_lr is specified
+        args.lr = args.blr * eff_batch_size / 256
+
+    print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
+    print("actual lr: %.2e" % args.lr)
+
+    print("accumulate grad iterations: %d" % args.accum_iter)
+    print("effective batch size: %d" % eff_batch_size)
+
+    # if args.distributed:
+        # model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
+        # model_without_ddp = model.module
+    
+    # following timm: set wd as 0 for bias and norm layers
+    param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
+    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
+    print(optimizer)
+    loss_scaler = NativeScaler()
+
+    misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
+
+    print(f"Start training for {args.epochs} epochs")
+    start_time = time.time()
+
+    model_target = None 
+    slow_target  = None 
+    for epoch in tqdm(range(args.start_epoch, args.epochs)):
+        if args.distributed:
+            data_loader_train.sampler.set_epoch(epoch)
+        
+        # 定 num_k == warmup_target_epochs 
+        if epoch == args.num_k: 
+            slow_target = models_mae.__dict__[args.target_model](norm_pix_loss=args.norm_pix_loss)  
+            slow_target.to(device) 
+
+            # 此时 model_target 的输出就是 (512, 64, 192), 所以说需要改一下 model 的网络架构了 
+            # 这两行可以保证 model_target 的结构和 model 一模一样了
+            model.decoder_pred = torch.nn.Linear(model.decoder_embed_dim, model.embed_dim, bias = True).to(device)
+            slow_target.decoder_pred = torch.nn.Linear(model.decoder_embed_dim, model.embed_dim, bias = True).to(device)     
+            slow_target.load_state_dict(model.state_dict())
+
+            model_target = models_mae.__dict__[args.target_model](norm_pix_loss=args.norm_pix_loss)  
+            model_target.to(device) 
+            model_target.decoder_pred = torch.nn.Linear(model.decoder_embed_dim, model.embed_dim, bias = True).to(device)     
+            model_target.load_state_dict(model.state_dict()) 
+
+        train_stats = train_one_epoch_new(
+            model, data_loader_train,
+            optimizer, device, epoch, loss_scaler,
+            log_writer=log_writer,
+            args=args, 
+            model_target = model_target, 
+            slow_target  = slow_target 
+        )
+        if args.output_dir and (epoch % 20 == 0 or epoch + 1 == args.epochs):
+            misc.save_model(
+                args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                loss_scaler=loss_scaler, epoch=epoch)
+
+        # 如果当前已经有 target_model 了，那么就对 target_model 做指数平滑更新
+        # 逐步更新 
+        # old-weight * 0.995 + new-weight * 0.005 
+        if model_target is not None: 
+            for param_target, param in zip(model_target.parameters(), model.parameters()):
+                param_target.data.copy_(param_target.data * (1.0 - args.tau) + param.data * args.tau)
+            
+        if slow_target is not None and epoch % args.num_k == 0: 
+            slow_target.load_state_dict(model.state_dict())
+
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                        'epoch': epoch,}
+
+        if args.output_dir and misc.is_main_process():
+            if log_writer is not None:
+                log_writer.flush()
+            with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
+                f.write(json.dumps(log_stats) + "\n")
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    # 加上 warmup_epochs 和 tau 
+    args.output_dir = os.path.join(
+        args.output_dir, 
+        str(args.warmup_target_epochs) + '_' + str(args.tau)
+    )
+    args.log_dir = os.path.join(
+        args.log_dir, 
+        str(args.warmup_target_epochs) + '_' + str(args.tau)
+    )
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
