ee44a000abca056a8c0c34f60de39390fcd2acdf
guangheli12 <3280939942@qq.com>
2024-04-19 17:04:49 +0000

    fix scripts

--- Bmae_eval_finetune.sh
+++ Bmae_eval_finetune.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main_finetune.py --finetune=bootstrap_pretrain_weights/5_0.005/checkpoint-199.pth --log_dir=bootstrap_finetune_logs --output_dir=bootstrap_finetune_weights --additional_info=5_0.005 --device=cuda:0
+python main_finetune.py --finetune=bootstrap_pretrain_weights/5_0.1/checkpoint-199.pth --log_dir=bootstrap_finetune_logs --output_dir=bootstrap_finetune_weights --additional_info=5_0.1 --device=cuda:0
--- Bmae_train.sh
+++ Bmae_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # EMA-MAE(best results for finetune evaluation)
-python ema_bootstrap_pretrain.py --tau=0.005 --warmup_target_epochs=5 --output_dir=bootstrap_pretrain_weights --log_dir=bootstrap_pretrain_logs --device=cuda:0
+python ema_bootstrap_pretrain.py --tau=0.1 --warmup_target_epochs=5 --output_dir=bootstrap_pretrain_weights --log_dir=bootstrap_pretrain_logs --device=cuda:0
 
 # EMA-MAE(best results for linear evaluation)
 python ema_bootstrap_pretrain.py --tau=0.005 --warmup_target_epochs=1 --output_dir=bootstrap_pretrain_weights --log_dir=bootstrap_pretrain_logs --device=cuda:0
